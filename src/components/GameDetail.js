208fcd03e80dfd861abc209e55e9e2826186c1a5
miralsuthar <57826091+miralsuthar@users.noreply.github.com>
2021-02-20 17:53:20 +0530

    some changes in game detail

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -5,6 +5,14 @@ import {motion} from 'framer-motion';
 //redux
 import {useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
+//IMAGE
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
+
 
 const GameDetail = ({pathId}) => {
     const history = useHistory();
@@ -15,6 +23,23 @@ const GameDetail = ({pathId}) => {
         if(element.classList.contains('shadow')){
             document.body.style.overflow = "auto";
             history.push("/");
+        }
+    }
+
+    const getPlatform = (platform) => {
+        switch(platform){
+            case "PlayStation 4":
+                return playstation;
+            case "Xbox One":
+                return xbox;
+            case "PC":
+                return steam;
+            case "Nintendo Switch":
+                return nintendo;
+            case "IOS":
+                return apple;
+            default:
+                return gamepad
         }
     }
     //data
@@ -33,7 +58,7 @@ const GameDetail = ({pathId}) => {
                         <h3>Platforms</h3>
                         <Platforms>
                             {game.platforms && game.platforms.map(data => (
-                                <h3 key = {data.platform.id}>{data.platform.name}</h3>
+                                <img key = {data.platform.id} src={getPlatform(data.platform.name)}></img>
                             ))}
                         </Platforms>
                     </Info>
